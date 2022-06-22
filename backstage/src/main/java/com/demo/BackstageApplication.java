d2ceb91f35b98753fdebd6d79ab1385bab0fdcf4
caster <victorcaster013@gmail.com>
2022-06-22 18:10:32 +0800

    finish

---
+++ backstage/src/main/java/com/demo/BackstageApplication.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Caster
+ * Date: 2022/5/5
+ * Comment:
+ */
+
+@SpringBootApplication
+public class BackstageApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackstageApplication.class,args);
+	}
+}
---
+++ restapi/src/main/java/com/demo/RestApiApplication.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Caster
+ * Date: 2022/5/5
+ * Comment:
+ */
+
+@SpringBootApplication
+public class RestApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+}
